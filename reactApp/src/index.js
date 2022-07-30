0f1c5f50763047730a2379f54b1ee88cf8bd5251
unknown <allielane24@gmail.com>
2022-07-30 17:37:24 +0200

    auth

--- reactApp/src/index.js
+++ reactApp/src/index.js
@@ -3,32 +3,45 @@ import ReactDOM from "react-dom";
 import { BrowserRouter, Route, Routes, Navigate, Link } from "react-router-dom";
 import { PublicPage, Movies, Profile, HomePage } from "./pages";
 
+import LoginPage from "./loginPage";
+import AuthProvider from "./authContext";
+import AuthHeader from "./authHeader";
+import ProtectedRoutes from "./protectedRoutes";
+
 const App = () => {
-  return (
-    <BrowserRouter>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/public">Public</Link>
-          </li>
-          <li>
-            <Link to="/movies">Movies</Link>
-          </li>
-          <li>
-            <Link to="/profile">Profile</Link>
-          </li>
-        </ul>
-        <Routes>
-          <Route path="/public" element={ <PublicPage /> } />
-          <Route path="/" element={ <HomePage /> } />
-          <Route path="/movies" element={ <Movies /> } />
-          <Route path="/profile" element={ <Profile /> } />
-          <Route path="*" element={ <Navigate to="/" /> } />
-        </Routes>
-    </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <AuthProvider>
+                <AuthHeader />
+                <ul>
+                    <li>
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/public">Public</Link>
+                    </li>
+                    <li>
+                        <Link to="/movies">Movies</Link>
+                    </li>
+                    <li>
+                        <Link to="/profile">Profile</Link>
+                    </li>
+                </ul>
+                <Routes>
+                    <Route path="/public" element={<PublicPage />} />
+                    <Route path="/" element={<HomePage />} />
+                    <Route path="/login" element={<LoginPage />} />
+
+                    <Route element={<ProtectedRoutes />}>
+                        <Route path="/movies" element={<Movies />} />
+                        <Route path="/profile" element={<Profile />} />
+                    </Route>
+
+                    <Route path="*" element={<Navigate to="/" />} />
+                </Routes>
+            </AuthProvider>
+        </BrowserRouter>
+    );
 };
 
 ReactDOM.render(<App />, document.getElementById("root"));
